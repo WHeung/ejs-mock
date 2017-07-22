87df8ef4a5ee7c80c73fd6c5942648cf2e0c0d71
ibone <water2212683@gmail.com>
2017-07-22 19:32:05 +0800

    ADD tempalte editor

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,11 +1,18 @@
+(function () {
+var editor1 = null;
+var editor2 = null;
+
 new Vue({
   el: '#app',
   data: {
-    list: []
+    list: [],
+    curMock: {},
+    curOption: {}
   },
   methods: {
     switchMockData: function (mock, option) {
       if (mock.responseKey === option.key) return
+      var self = this
       $.ajax({
         url: './update',
         data: {
@@ -15,8 +22,20 @@ new Vue({
         dataType: 'json',
         success: function (data) {
           mock.responseKey = option.key
+          self.curOption = option
         }
       })
+    },
+    switchMock: function (mock) {
+      this.curMock = mock
+      this.curOption = mock.responseOptions[mock.responseKey]
+    }
+  },
+  watch: {
+    curOption: function (val, oldVal) {
+      if (editor1) {
+        editor1.setValue(val.template)
+      }
     }
   },
   mounted: function () {
@@ -26,7 +45,31 @@ new Vue({
       dataType: 'json',
       success: function (data) {
         self.list = data.data
+        self.curMock = self.list[0]
+        self.curOption = self.curMock.responseOptions[self.curMock.responseKey]
       }
     })
   }
 })
+
+editor1 = CodeMirror.fromTextArea(document.getElementById("editor1"), {
+  mode: "application/json",
+  gutters: ["CodeMirror-lint-markers"],
+  lint: true,
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  theme: 'material'
+})
+
+editor2 = CodeMirror.fromTextArea(document.getElementById("editor2"), {
+  mode: "application/json",
+  gutters: ["CodeMirror-lint-markers"],
+  lint: true,
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  theme: 'material'
+})
+
+})()
